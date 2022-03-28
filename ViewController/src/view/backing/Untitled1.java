051bda98238767dd942769523a88f908f48314a5
Nawwar <Nawwar@192.168.1.6>
2022-03-28 21:57:55 +0300

    mm

---
+++ ViewController/src/view/backing/Untitled1.java
@@ -0,0 +1,35 @@
+package view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+
+public class Untitled1 {
+    private RichPanelGroupLayout pgl1;
+    private RichForm f1;
+    private RichDocument d1;
+
+    public void setPgl1(RichPanelGroupLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGroupLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+}
